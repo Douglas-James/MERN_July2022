2f24a56737fbd4513a72c064c2532f91fe925b92
Douglas-James <ryle@drumclog.com>
2022-07-09 23:53:29 -0500

    'update '

---
+++ React/CLass_Components/class-components/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import MyNewComponent from './MyNewComponent';
+function App() {
+  return (
+    <div className="App">
+        <MyNewComponent />
+        <MyNewComponent />
+    </div>
+  );
+}
+
+export default App;
