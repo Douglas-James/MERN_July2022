2772b7587d8065363f3e3344f3046c09a7db44ad
Douglas-James <ryle@drumclog.com>
2022-07-10 23:10:11 -0500

    update

--- React/CLass_Components/props/first-props/src/App.js
+++ React/CLass_Components/props/first-props/src/App.js
@@ -4,7 +4,22 @@ import Menuitem from './Components/MenuItem';
 function App() {
   return (
     <div className="App">
-          <Menuitem></Menuitem> 
+      <h1>Dojo Dinner Menu</h1>
+      <Menuitem
+        dishName={"Basile Calamari-Thai"}
+        price={12.0}
+        description={"Fried sequid tho"}
+      ></Menuitem>
+      <Menuitem
+        dishName={"Strogonoff"}
+        price={15.0}
+        description={"Pasta tho"}
+      ></Menuitem>
+      <Menuitem
+        dishName={"Buffalo Wings"}
+        price={8.0}
+        description={'Its fried sequid with basil and Thai spices'}
+      ></Menuitem>
     </div>
   );
 }
--- React/CLass_Components/props/first-props/src/Components/MenuItem.jsx
+++ React/CLass_Components/props/first-props/src/Components/MenuItem.jsx
@@ -5,10 +5,9 @@ class Menuitem extends Component {
   render() {
     return (
       <div className="item">
-        <h1>Dojo Dinner Menu</h1>
-        <h3>Basil Calamari- Thai Style</h3>
-        <p>Price: $12.00</p>
-        <p>Description: its friend sequid with some basil and thai spices</p>
+        <h3>Name: {this.props.dishName}</h3>
+        <p>Price: ${this.props.price}</p>
+        <p>Description: {this.props.description}</p>
       </div>
     );
   }
