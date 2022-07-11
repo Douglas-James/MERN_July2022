f6847ac02df6ee619b345784165cf24434b3ee1d
Douglas-James <ryle@drumclog.com>
2022-07-11 00:16:36 -0500

    childer 1

---
+++ React/CLass_Components/childern/README.md
@@ -0,0 +1,3 @@
+# Children
+Components can also have children. Children of components are those element(s)/component(s) that are within the two "tags" of JSX elements.
+
--- React/CLass_Components/childern/first-children/src/App.js
+++ React/CLass_Components/childern/first-children/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import MyNewComponent from "./Component/MyNewComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyNewComponent header={"Header Prop"}>
+        <p>This is a child</p>
+        <p>This is another child</p>
+        <p>This is even another child</p>
+      </MyNewComponent>
     </div>
   );
 }
