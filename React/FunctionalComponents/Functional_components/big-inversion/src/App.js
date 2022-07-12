d6afc45614903d239189a5719bf3a3fafa989930
Douglas-James <ryle@drumclog.com>
2022-07-11 22:28:48 -0500

    hairColor fix

--- React/FunctionalComponents/Functional_components/big-inversion/src/App.js
+++ React/FunctionalComponents/Functional_components/big-inversion/src/App.js
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <PersonCard firstName={"Jane"} lastName={"Does"} age={45} hairColor={"Black"}/>
       <PersonCard firstName={"John"}lastName={"Smith"} age={88} hairColor={"Brown"}/>
-      <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={50} HairColor={"Black"}/>
-      <PersonCard firstName={"Maria"} lastName={"Smith"} age={62} HairColor={"Black"}/>
+      <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Black"}/>
+      <PersonCard firstName={"Maria"} lastName={"Smith"} age={62} hairColor={"Black"}/>
     </div>
   );
 }
