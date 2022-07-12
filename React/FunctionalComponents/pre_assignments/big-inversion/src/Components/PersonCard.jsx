d12990f68006f32bfde7424bec1ebf3614226b59
Douglas-James <ryle@drumclog.com>
2022-07-12 08:53:19 -0500

    change update state

--- React/FunctionalComponents/pre_assignments/big-inversion/src/Components/PersonCard.jsx
+++ React/FunctionalComponents/pre_assignments/big-inversion/src/Components/PersonCard.jsx
@@ -2,21 +2,22 @@
 // agecrement = (e) => {
 //   this.setState({ age: this.state.age + 1 });
 // };
+// need useState
 import React, {useState} from 'react';
 const PersonCard = props => 
 {
-  let [stateAge, stateage] = useState(props.age)
+  let [state , setState] = useState(props.age)
   const agecrement = () => 
   {
       // let state =  setState({ age: props.state.age + 1 });
-    stateage(stateAge+1)
+    setState(state+1)
   };
   return (
     <div>
       <h1>{props.lastName},{props.firstName}</h1>
-      {stateAge}
+      {state}
       <p>Hair color:{props.hairColor}</p>
-      <button onClick={agecrement}>Clik to incress Age</button>
+      <button onClick={agecrement}>Click to incress Age</button>
     </div>
   );
 };
