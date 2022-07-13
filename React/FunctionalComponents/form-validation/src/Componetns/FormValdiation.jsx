c3a0402d45d449c0204c5ac84c58820400e95fbe
Douglas-James <ryle@drumclog.com>
2022-07-13 13:21:22 -0500

    only for email validation

--- React/FunctionalComponents/form-validation/src/Componetns/FormValdiation.jsx
+++ React/FunctionalComponents/form-validation/src/Componetns/FormValdiation.jsx
@@ -1,34 +1,67 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import validator from 'validator'
+// // creat variable object or anythings up here
+//  const initialState = {
+//        firstName: {
+//        value: '',
+//        error: null
+//      },
+//      lastName: {
+//        value: '',
+//        error: null
+//      },
+//      email: {
+//          value: '',
+//          error: null
+//      }
+//   };
+//   // function here
+//   const reducer = (state, action) => 
+//   {
+//    return{
+//      ...state,
+//      [action.type]: action.playload
+//    };
+//   }
   
-export default () => {
-
-  
+  export default () => {
+  // validation email
   const [emailError, setEmailError] = useState('')
+  // function for validation
   const validateEmail = (e) => {
     var email = e.target.value
   
     if (validator.isEmail(email)) {
       setEmailError('Valid Email :)')
     } else {
-      setEmailError('Enter valid Email!')
+      setEmailError('Email is invalid!')
     }
   }
+
+  // // funcgtion for reducer and initial state
+  // const [state, dispatch] = useReducer(reducer, initialState);
+  // const handleChange = (e) => {
+  //   const {name, value} = e.target;
+  //   dispatch({
+  //     type: name,
+  //     playload: value
+  //   })
+  // }
+
+  
   
   return (
     <div style={{
       margin: 'auto',
       marginLeft: '300px',
     }}>
-      <pre>
-        <h2>Validating Email in ReactJS</h2>
-        <span>Enter Email: </span><input type="text" id="userEmail" 
-        onChange={(e) => validateEmail(e)}></input> <br />
+      <form>
+         <span>Enter Email: </span><input type="email" id="userEmail" name="email" onChange={(e) => validateEmail(e)} ></input> <br />
         <span style={{
           fontWeight: 'bold',
           color: 'red',
         }}>{emailError}</span>
-      </pre>
+      </form>
     </div>
   );
 }
