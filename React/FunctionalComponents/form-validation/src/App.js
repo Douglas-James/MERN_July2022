7d8f1e62abb06b141f6065e75443491902de1caf
Douglas-James <ryle@drumclog.com>
2022-07-13 12:28:27 -0500

    using email validation

--- React/FunctionalComponents/form-validation/src/App.js
+++ React/FunctionalComponents/form-validation/src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import FormValidation from './Components/FormValidation';
-function App() {
+import React, { useState } from "react";
+import validator from 'validator'
+  
+const App = () => {
+  
+  const [emailError, setEmailError] = useState('')
+  const validateEmail = (e) => {
+    var email = e.target.value
+  
+    if (validator.isEmail(email)) {
+      setEmailError('Valid Email :)')
+    } else {
+      setEmailError('Enter valid Email!')
+    }
+  }
+  
   return (
-    <div className="App">
-     <FormValidation></FormValidation>
+    <div style={{
+      margin: 'auto',
+      marginLeft: '300px',
+    }}>
+      <pre>
+        <h2>Validating Email in ReactJS</h2>
+        <span>Enter Email: </span><input type="text" id="userEmail" 
+        onChange={(e) => validateEmail(e)}></input> <br />
+        <span style={{
+          fontWeight: 'bold',
+          color: 'red',
+        }}>{emailError}</span>
+      </pre>
     </div>
   );
 }
-
-export default App;
+  
+export default App
