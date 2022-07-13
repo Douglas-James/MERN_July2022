c618db869a2d7f5176eec955860ae98368de2a07
Douglas-James <ryle@drumclog.com>
2022-07-12 23:35:15 -0500

    'userreduce'

---
+++ React/FunctionalComponents/useReducer-project/src/Components/UserReducer.jsx
@@ -0,0 +1,50 @@
+// useReducer import
+import React, { useReducer} from 'react';
+
+const initalState ={
+  name: '',
+  email: ''
+};
+const reducer = (state, action) => {
+  return{
+    ...state,
+    [action.type]: action.payload
+    };
+}
+
+export default () => {
+    const [state, dispatch] = useReducer(reducer, initalState);
+ 
+    function handleChange(e) {
+        const { name, value } = e.target;
+        dispatch({
+            type: name,
+            payload: value
+        });
+    }
+    return (
+        <div>
+            {JSON.stringify(state)}
+            <div>
+                <label>
+                    <span>Name:</span>{' '}
+                    <input
+                        name="name"
+                        value={state.name}
+                        onChange={handleChange}
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    <span>Email:</span>{' '}
+                    <input
+                        name="email"
+                        value={state.email}
+                        onChange={handleChange}
+                    />
+                </label>
+            </div>
+        </div>
+    );
+}
