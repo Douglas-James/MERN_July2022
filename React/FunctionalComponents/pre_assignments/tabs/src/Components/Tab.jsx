5fba19fd2f7f389020144b708956c8bb6d461011
Douglas-James <ryle@drumclog.com>
2022-07-14 09:33:35 -0500

    'tabs pre_assisgnment'

---
+++ React/FunctionalComponents/pre_assignments/tabs/src/Components/Tab.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import './tabstyle.css'
+const Tab = (props) => {
+  // style for p inline block
+  const tabStyle = {
+      display: 'inline-block',
+    }
+  // tab state track
+  const [tab, setTab] = useState(0);
+
+  // hand the onchange 
+  const handTab = (e, idx) => {
+      setTab(idx);
+  }
+  return (
+    <div>
+        {/* Map the tabs */}
+        {
+          props.selectTab.map((tab, idx) => {
+            return(
+              <div style={tabStyle} >
+                  <p key={idx}  className='tabs' onClick={(e)=> handTab(e, idx)}>{tab.label}</p>
+              </div>
+            )
+          })
+        }
+        {/* Show contents */}
+        <div>
+          {
+            <p>{props.selectTab[tab].content}</p>
+          }
+        </div>
+    </div>
+  );
+};
+export default Tab;
