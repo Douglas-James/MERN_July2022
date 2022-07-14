c08d1726d7e999f0c38b3ac6a084fdb35cf2616f
Douglas-James <ryle@drumclog.com>
2022-07-13 19:05:34 -0500

    remove fewthings

--- React/FunctionalComponents/pre_assignments/box-generator/src/Components/Box.jsx
+++ React/FunctionalComponents/pre_assignments/box-generator/src/Components/Box.jsx
@@ -5,8 +5,8 @@ const Box = props => {
 
   const boxStyle = {
     display: 'inline-block',
-    height: `${props.size**3}px`,
-    width: `${props.size**3}px`,
+    height: `${props.size**2}px`,
+    width: `${props.size**2}px`,
     backgroundColor: props.color,
     margin: '0px'
   }
