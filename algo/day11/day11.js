d5e310aed294cb770a20f281eee5fde1055a9f01
Douglas-James <ryle@drumclog.com>
2022-07-20 10:55:51 -0500

    'algo morning'

---
+++ algo/day11/day11.js
@@ -0,0 +1,65 @@
+const object1 = {
+    closedCreditMemos: [],
+    closedDeliveryOrders: [],
+    closedPickupOrders: [
+        { id: 112, type: "pickup" },
+        { id: 117, type: "pickup" },
+    ],
+    openCreditMemos: [],
+    openDeliveryOrders: [
+        {
+            id: 123,
+            type: "delivery",
+            gateCode: "#2552",
+        },
+        {
+            id: 153,
+            type: "delivery",
+            instructions: "Place in secure delivery box.",
+        },
+    ],
+    openPickupOrders: [
+        {
+            id: 123,
+            type: "pickup",
+        },
+    ],
+    returnPickupOrders: [],
+};
+
+const expected1 = [
+    { id: 112, type: "pickup" },
+    { id: 117, type: "pickup" },
+    { id: 123, type: "delivery" },
+    { id: 153, type: "delivery" },
+    { id: 123, type: "pickup" },
+];
+
+/**
+ * Takes an object containing arrays of objects and places all the nested
+ * objects into a new one-dim array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Object} o Containing arrays of objects.
+ * @returns {Object[]} An array of objects.
+ */
+function flattenObjectOfArrays(obj) { 
+    let result = [];
+    
+    for(let key in obj){
+        // console.log(object);
+        // console.log(obj[object]);
+
+        //if array is empty then we go to the next
+        if(obj[key].length != 0){
+            for(item in obj[key]){
+                // console.log(obj[key][item]);
+                result.push(obj[key][item]);
+            }
+        }
+    }
+
+    return result;
+}
+
+console.log(flattenObjectOfArrays(object1));
